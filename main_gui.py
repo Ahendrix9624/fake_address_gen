5d5cc2f8bbc282e4407920f3dad8a4c829f2bfa0
Drew <33384698+Ahendrix9624@users.noreply.github.com>
2023-03-11 14:04:35 -0700

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,31 +1,12 @@
-#!/usr/bin/env python3
-#
-#  [Program]
-#
-#  Fake Name/Address Generator GUI Version
-#
-#  [Author]
-#
-#  Drew, https://github.com/Ahendrix9624/
-#
-#  [License]
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#  See 'LICENSE' for more information.
-
+"""
+USAGE - This code generates fake name and address data using the Faker library and displays 
+        it in a GUI window created using the tkinter library. 
+        The "Generate Fake Data" button triggers the data generation and displays it in a text box. 
+        The generated data is also copied to the clipboard using the pyperclip library. 
+        If the text box is not empty, the data generation is repeated until an empty text box is generated.
+        
+AUTHOR - https://github.com/Ahendrix9624/
+"""
 from faker import Faker 
 from tkinter import *
 import pyperclip
